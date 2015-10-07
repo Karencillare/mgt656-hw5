9c3776b8cfa998fd1badb1e4afdbc69f375278c6
karencillare <karen.ramirezestrada@yale.edu>
2015-10-07 23:17:47 +0000

    Gleaming-dinosaur

--- server.js
+++ server.js
@@ -5,4 +5,8 @@ app.get('/', function(request, response){
     response.send('Hello MGT-656!');
 });
 
+app.get('/gleaming-dinosaur', function(request, response){
+    response.send('The gleaming-dinosaur wants to be Pusheen!');
+});
+
 app.listen(process.env.PORT || 4000);
